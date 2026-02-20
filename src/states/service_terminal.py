9973fab46629c6466614a9f72f705a89267c8801
mirek <miroslav.binas@tuke.sk>
2026-02-20 15:03:46 +0100

    zavolanie enter() predka v service terminali

--- src/states/service_terminal.py
+++ src/states/service_terminal.py
@@ -13,6 +13,7 @@ class ServiceTerminal(BaseState):
     name = 'Service Terminal'
 
     def enter(self):
+        super().enter()
         self.context.diag_led.set_color(5, 5, 5)
     #     self.parser = Parser(self.device)
     #
