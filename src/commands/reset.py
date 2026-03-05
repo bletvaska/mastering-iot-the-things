bac1c2ce17fac2d533a39d91976dc6fa4828985c
mirek <miroslav.binas@tuke.sk>
2026-03-05 11:47:18 +0100

    aktualizovany prikaz reset

--- src/commands/reset.py
+++ src/commands/reset.py
@@ -1,14 +1,27 @@
 from time import sleep
 
-from machine import reset
+from machine import reset, soft_reset
 
 from commands.base import BaseCommand
 
 class Reset(BaseCommand):
     name = 'reset'
     description = 'Soft/Hard reset'
+    usage = ('reset soft   Soft reset.\n'
+             'reset hard   Hard reset.\n')
 
     def exec(self) -> None:
-        print('Going to reset')
-        sleep(0.1)
-        reset()
+        if len(self.params) != 1 or self.params[0] not in ['soft', 'hard']:
+            print('Wrong usage.')
+            print(self.usage)
+            return
+
+        if self.params[0] == 'soft':
+            print('Soft reset.')
+            sleep(0.1)
+            soft_reset()
+
+        elif self.params[0] == 'hard':
+            print('Hard reset.')
+            sleep(0.1)
+            reset()
