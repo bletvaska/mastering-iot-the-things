8f19f08bab9102642783ba2e1e743d9c02880f13
mirek <miroslav.binas@tuke.sk>
2026-03-06 15:07:40 +0100

    refactoring

--- src/states/publish.py
+++ src/states/publish.py
@@ -1,22 +1,20 @@
 import json
-
 import os
 from machine import RTC
-
-from helpers import to_iso8601
 
 from constants import METRICS_FILE
 from models.message import Message
 from models.metric import Metric
 from .sleep import Sleep
 from .base import BaseState
+from helpers import to_iso8601
 
 
 class Publish(BaseState):
     name = "Publish"
 
-    def exec(self):
-        metrics = []
+    def enter(self) -> None:
+        self.metrics = []
 
         with open(METRICS_FILE, 'r') as file:
             for line in file:
@@ -27,7 +25,7 @@ class Publish(BaseState):
                     value=float(parts[1]),
                     units=parts[2]
                 )
-                metrics.append(temperature)
+                self.metrics.append(temperature)
 
                 humidity = Metric(
                     dt=parts[0],
@@ -35,16 +33,18 @@ class Publish(BaseState):
                     value=float(parts[3]),
                     units='%'
                 )
-                metrics.append(humidity)
+                self.metrics.append(humidity)
 
+    def exec(self):
         message = Message(
             dt=to_iso8601(RTC().datetime()),
-            metrics=metrics
+            metrics=self.metrics
         )
 
         topic = f'{self.context.settings.base_topic()}/data'
         self.context.mqtt_client.publish(topic, json.dumps(message.model_dump()))
 
-        os.unlink(METRICS_FILE)
-
         return Sleep(self.context)
+
+    def exit(self) -> None:
+        os.unlink(METRICS_FILE)
