0ca7d836934f11b4e2c982c053d379989198a14e
mirek <miroslav.binas@tuke.sk>
2026-03-06 13:12:28 +0100

    fixnuty last will

--- src/states/connect_network.py
+++ src/states/connect_network.py
@@ -10,7 +10,8 @@ from helpers import do_connect
 
 def on_message(topic: bytes, message: bytes):
     topic = topic.decode('utf-8')
-    message = json.loads( message )
+    print(f'>> Processing message: {message}')
+    message = json.loads(message)
     print(f'>>> {topic}: {message}')
 
 
@@ -31,23 +32,37 @@ class ConnectNetwork(BaseState):
             port=mqtt.port,
             user=mqtt.username,
             password=mqtt.password,
-            keepalive=0,
+            keepalive=10,
             ssl=not mqtt.insecure
         )
         client.set_callback(on_message)
+
+        client.set_last_will(
+            f'{settings.base_topic()}/status',
+            '{"status": "offline", "owner": "mirek"}',
+            retain=True
+        )
+
         client.connect()
 
-        client.publish(f'{settings.base_topic()}/status', 'online')
+        client.publish(
+            f'{settings.base_topic()}/status',
+            '{"status": "online", "owner": "mirek"}',
+            retain=True
+        )
         client.subscribe(f'{settings.base_topic()}/cmd')
 
         print('>> Waiting for messages...')
         while True:
             client.check_msg()
+            client.ping()
             sleep(0.2)
 
+        client.publish(
+            f'{settings.base_topic()}/status',
+            '{"status": "offline", "owner": "mirek"}',
+            retain=True
+        )
         client.disconnect()
 
         return Sleep(self.context)
-
-
-
