76e4af8a65650d51fff7fc06ac79a6399f84e8d3
mirek <miroslav.binas@tuke.sk>
2026-03-17 14:12:52 +0100

    monkey patching pre modul `pathlib`
    * funkcia `open()` nevie nacitat objekt typu `Path`
      * monkey patch na explicitne zavolanie `str()` parametra s cestou

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 # pins configuration
 DHT_PIN = 3
 BTN_PIN = 20
@@ -12,13 +14,11 @@ I2C_SCL_PIN = 5  # I2C clock line (SCL)
 RTC_ALARM_PIN = 2  # GPIO pin for RTC alarm signal
 
 # paths
-DATA_FOLDER = '/data'
-SETTINGS_FILE = f'{DATA_FOLDER}/settings.json'
-METRICS_FILE = f'{DATA_FOLDER}/metrics.csv'
-SALT_FILE = f'{DATA_FOLDER}/.salt'
+DATA_FOLDER = Path('/data')
+SETTINGS_FILE = DATA_FOLDER / 'settings.json'
+METRICS_FILE = DATA_FOLDER / 'metrics.csv'
+SALT_FILE = DATA_FOLDER / '.salt'
 
 # durations
 SHORT_PRESS_DURATION = 2 * 1000
 LONG_PRESS_DURATION = 6 * 1000
-
-# temp units
--- src/main.py
+++ src/main.py
@@ -2,5 +2,18 @@ from states.init import Init
 from context import Context
 
 if __name__ == "__main__":
+    # monkey-patching of builtin open() for reading the Path objects
+    import builtins
+
+    _original_open = builtins.open
+
+    def _patched_open(file, *args, **kwargs):
+        if not isinstance(file, (str, bytes)):
+            file = str(file)
+        return _original_open(file, *args, **kwargs)
+
+    builtins.open = _patched_open
+
+    # run
     thsensor = Context(Init)
     thsensor.run()
